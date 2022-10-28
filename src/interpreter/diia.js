145dc55c449b46e83d589b80ec301e7e4b552686
Davyd Kohut <davyd.kohut@gmail.com>
2022-10-28 08:43:12 +0300

    0.0.9: lambda & async & more

--- src/interpreter/diia.js
+++ src/interpreter/diia.js
@@ -1,30 +1,60 @@
-import { DiiaContext } from "./context.js";
+import { DiiaContext, LambdaContext } from "./context.js";
 
-class Diia {
-    constructor(context, name, parameters, body) {
-        this.context = context;
-        this.name = name;
-        this.parameters = parameters;
-        this.body = body;
+export class NamedArguments {
+    constructor(args = {}) {
+        Object.assign(args);
     }
+}
 
-    call(args) {
-        const diiaContext = new DiiaContext(this.context);
+export function makeDiia(context, name, parameters, body, async) {
+    const diiaFn = function (args) {
+        const diiaContext = new DiiaContext(context);
+        diiaContext.set('__diia__', diiaContext);
+        diiaContext.set('__async__', async);
 
         diiaContext.set('аргументи', args);
 
         if (Array.isArray(args)) {
-            this.parameters.forEach((p, i) => {
+            parameters.forEach((p, i) => {
                 diiaContext.set(p, args[i]);
             });
-        } else if (typeof args === "object") {
-            this.parameters.forEach((p) => {
+        } else if (args instanceof NamedArguments) {
+            parameters.forEach((p) => {
                 diiaContext.set(p, args[p]);
             });
+        } else {
+            diiaContext.set(parameters[0], args);
         }
 
-        return diiaContext.run(this.body);
+        return diiaContext.run(body);
     }
+
+    diiaFn.__diia_fn__ = true;
+
+    return diiaFn;
 }
 
-export default Diia;
+
+export function makeLambda(context, parameters, expression) {
+    const lambdaFn = function (args) {
+        const lambdaContext = new LambdaContext(context);
+
+        if (Array.isArray(args)) {
+            parameters.forEach((p, i) => {
+                lambdaContext.set(p, args[i]);
+            });
+        } else if (args instanceof NamedArguments) {
+            parameters.forEach((p) => {
+                lambdaContext.set(p, args[p]);
+            });
+        } else {
+            lambdaContext.set(parameters[0], args);
+        }
+
+        return lambdaContext.run([expression]);
+    }
+
+    lambdaFn.__lambda_fn__ = true;
+
+    return lambdaFn;
+}
