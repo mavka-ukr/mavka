05e57a5e9005b95d923ee78e3ce29c046bf19551
Davyd Kohut <davyd.kohut@gmail.com>
2023-03-14 15:14:37 +0200

    0.9.24

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ import ProxyCell from "./interpreter/cells/proxyCell.js";
  * @property {Loader} loader
  */
 class Mavka {
-  static VERSION = "0.9.22";
+  static VERSION = "0.9.24";
 
   constructor(options = {}) {
     this.arithmeticInstruction = new ArithmeticInstruction(this);
