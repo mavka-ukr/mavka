1b56374b818704f357c0ac4634e5c82dd4a9a219
ArtemiiKravchuk <115405408+ArtemiiKravchuk@users.noreply.github.com>
2023-03-27 13:44:18 +0300

    Додати більше функції для списків
    1. список.сортувати(). Повертає відсортований список. Числа сортуються в порядку збільшення, строки - за Юнікодом.
    2. список.розвернути(). Розвертає список. Перший елемент стає останнім, а останній - першим.
    3. список.поєднати(арг1). Поєднує разом всі елементи списку в одну строку, розділяючи їх арг1. Якщо арг1 не вказано, то не розділяє елементи.

--- src/interpreter/cells/listStructureCell.js
+++ src/interpreter/cells/listStructureCell.js
@@ -44,6 +44,22 @@ class ListStructureCell extends StructureCell {
     this.setMethod("додати", (args, context, options) => {
       return this.mavka.makeNumber(options.meValue.properties.values.push(Object.values(args)[0]));
     });
+    this.setMethod("сортувати", (args, context, options) => {
+      return this.mavka.makeList(options.meValue.properties.values.sort(function(a, b) {
+        return a.asJsValue(context) - b.asJsValue(context);
+      }));
+    });
+    this.setMethod("розвернути", (args, context, options) => {
+      return this.mavka.makeList(options.meValue.properties.values.reverse());
+    });
+    this.setMethod("поєднати", (args, context, options) => {
+      if (args[0]==undefined) {
+        var dlm = "";
+      } else {
+        var dlm = args[0].asJsValue(context);
+      }
+      return this.mavka.makeText(options.meValue.properties.values.map((element) => element.asJsValue(context)).join(dlm));
+    });
   }
 
   static createInstance(mavka) {
