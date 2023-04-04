bb7db9a2247470326e6927a61397d959f14100f4
Davyd Kohut <davyd.kohut@gmail.com>
2023-04-04 13:10:06 +0300

    0.10.23

--- src/main.js
+++ src/main.js
@@ -121,7 +121,7 @@ import FileStructureCell from "./library/fileStructureCell.js";
  * @property {Loader} loader
  */
 class Mavka {
-  static VERSION = "0.10.22";
+  static VERSION = "0.10.23";
 
   constructor(options = {}) {
     if (!options.global) {
