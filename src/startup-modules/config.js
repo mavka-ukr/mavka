322fad02b875e7d1725e796136af84859f834490
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-04-04 13:32:40 +0300

    Move to config.js

---
+++ src/startup-modules/config.js
@@ -0,0 +1,10 @@
+const driveLetterPattern = /^\/[A-Z]:\//;
+let startupModuleDirectory = new URL('./', import.meta.url).pathname;
+
+if (driveLetterPattern.test(startupModuleDirectory)) {
+  startupModuleDirectory = startupModuleDirectory.substring(1);
+}
+
+startupModuleDirectory = startupModuleDirectory.replaceAll("\\", "/");
+
+export default startupModuleDirectory;
