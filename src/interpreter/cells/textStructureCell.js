7a89fb4c8bee2a34464b7e5dca60219f3c118f4b
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-04-08 20:23:09 +0300

    Implement text structure methods

--- src/interpreter/cells/textStructureCell.js
+++ src/interpreter/cells/textStructureCell.js
@@ -154,6 +154,340 @@ class TextStructureCell extends StructureCell {
 
       return options.meValue.asNumber(context).doCompareLesserThanOrEquals(context, value);
     });
+    this.setMethod("довжина", (args, context, options) => {
+      return this.mavka.toCell(options.meValue.asJsValue(context).length);
+    });
+    this.setMethod("виконати_отримання_елемента", (args, context, options) => {
+      let index = Object.values(args)[0];
+
+      try {
+        if (this.mavka.isEmpty(index) || typeof index === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиціяʼ не є пусто."));
+        }
+
+        index = index.asNumber(context).asJsValue(context);
+
+        if (index < 0) {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиціяʼ є не менше 0."));
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼпозиціяʼ у число."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value[index]);
+    });
+    this.setMethod("символ", (args, context, options) => {
+      let index = args[0];
+
+      try {
+        if (this.mavka.isEmpty(index) || typeof index === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиціяʼ не є пусто."));
+        }
+
+        index = index.asNumber(context).asJsValue(context);
+
+        if (index < 0) {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиціяʼ є не менше 0."));
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼпозиціяʼ у число."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.charAt(index));
+    });
+    this.setMethod("код_символу", (args, context, options) => {
+      let index = args[0];
+
+      try {
+        if (this.mavka.isEmpty(index) || typeof index === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиціяʼ не є пусто."));
+        }
+
+        index = index.asNumber(context).asJsValue(context);
+
+        if (index < 0) {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиціяʼ є не менше 0."));
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼпозиціяʼ у число."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.charCodeAt(index));
+    });
+    this.setMethod("позиція", (args, context, options) => {
+      let searchString = args[0];
+
+      try {
+        if (this.mavka.isEmpty(searchString) || typeof searchString === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼзначенняʼ не є пусто."));
+        }
+
+        searchString = searchString.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼзначенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.indexOf(searchString));
+    });
+    this.setMethod("повторити", (args, context, options) => {
+      let count = args[0];
+
+
+      try {
+        if (this.mavka.isEmpty(count) || typeof count === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼкількістьʼ не є пусто."));
+        }
+
+        count = count.asNumber(context).asJsValue(context);
+
+        if (Number.isNaN(count)) {
+          throw new TypeError("Expected `count` to not be NaN");
+        }
+
+        if (count < 0) {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼкількістьʼ є не менше 0."));
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼкількістьʼ у число."));
+        } else {
+          throw e;
+        }
+      }
+
+      if (count < 0) {
+        this.mavka.fall(context, this.mavka.makeText("Очікується, що кількість не меншою від 0."));
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.repeat(count));
+    });
+    this.setMethod("замінити_перше", (args, context, options) => {
+      let [pattern, replacement] = args;
+
+      try {
+        if (this.mavka.isEmpty(pattern) || typeof pattern === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼстаре_значенняʼ не є пусто."));
+        }
+
+        pattern = pattern.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼстаре_значенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      try {
+        if (this.mavka.isEmpty(replacement) || typeof replacement === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼнове_значенняʼ не є пусто."));
+        }
+
+        replacement = replacement.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼнове_значенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.replace(pattern, replacement));
+    });
+    this.setMethod("замінити", (args, context, options) => {
+      let [pattern, replacement] = args;
+
+      try {
+        if (this.mavka.isEmpty(pattern) || typeof pattern === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼстаре_значенняʼ не є пусто."));
+        }
+
+        pattern = pattern.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼстаре_значенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      try {
+        if (this.mavka.isEmpty(replacement) || typeof replacement === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼнове_значенняʼ не є пусто."));
+        }
+
+        replacement = replacement.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼнове_значенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.replaceAll(pattern, replacement));
+    });
+    this.setMethod("зрізати", (args, context, options) => {
+      let [indexStart, indexEnd] = args;
+
+      try {
+        if (this.mavka.isEmpty(indexStart) || typeof indexStart === "undefined") {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиція_відʼ не є пусто."));
+        }
+
+        indexStart = indexStart.asNumber(context).asJsValue(context);
+
+        if (indexStart < 0) {
+          this.mavka.fall(context, this.mavka.makeText("Очікується, що ʼпозиція_відʼ є не менше 0."));
+        }
+
+        if (Number.isNaN(indexStart)) {
+          throw new TypeError("Expected `indexStart` to not be NaN");
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼпозиція_відʼ у число."));
+        } else {
+          throw e;
+        }
+      }
+
+      try {
+        if (!(this.mavka.isEmpty(indexEnd) || typeof indexEnd === "undefined")) {
+          indexEnd = indexEnd.asNumber(indexEnd).asJsValue(context);
+          
+          if (Number.isNaN(indexEnd)) {
+            throw new TypeError("Expected `indexEnd` to not be NaN");
+          }
+
+          if (indexEnd < indexStart) {
+            this.mavka.fall(context, this.mavka.makeText(`Очікується, що ʼпозиція_доʼ є не менше від ʼпозиція_відʼ (${indexStart}).`));
+          }
+        } else {
+          indexEnd = undefined;
+        }
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼпозиція_доʼ у число."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.substring(indexStart, indexEnd));
+    });
+    this.setMethod("приєднати", (args, context, options) => {
+      if (args.length === 0) {
+        return options.meValue;
+      }
+      try {
+        args = args.map(v => v.asText(context).asJsValue(context));
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити деякі параметри ʼінші_текстиʼ у текст."))
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.concat(...args));
+    });
+    this.setMethod("розділити", (args, context, options) => {
+      let separator = args[0];
+
+      try {
+        separator = separator.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼроздільникʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.makeList(
+        options.meValue.meta.value.split(separator).map(v => 
+          this.mavka.makeText(v)));
+    });
+    this.setMethod("починається_з", (args, context, options) => {
+      let searchString = args[0];
+
+      try {
+        searchString = searchString.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼзначенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.startsWith(searchString));
+    });
+    this.setMethod("закінчується_на", (args, context, options) => {
+      let searchString = args[0];
+
+      try {
+        searchString = searchString.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити параметр ʼзначенняʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.endsWith(searchString));
+    });
+    this.setMethod("обрізати", (args, context, options) => {
+      return this.mavka.toCell(options.meValue.meta.value.trim());
+    });
+    this.setMethod("виконати_порівняння_чи_містить", (args, context, options) => {
+      let searchString = args[0];
+
+      try {
+        searchString = searchString.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити значення після ʼміститьʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(options.meValue.meta.value.includes(searchString));
+    });
+    this.setMethod("виконати_порівняння_чи_не_містить", (args, context, options) => {
+      let searchString = args[0];
+
+      try {
+        searchString = searchString.asText(context).asJsValue(context);
+      } catch (e) {
+        if (e instanceof TypeError) {
+          this.mavka.fall(context, this.mavka.makeText("Не вдалось перетворити значення після ʼміститьʼ у текст."));
+        } else {
+          throw e;
+        }
+      }
+
+      return this.mavka.toCell(!options.meValue.meta.value.includes(searchString));
+    });
   }
 
   static createInstance(mavka) {
