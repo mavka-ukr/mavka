82fe2be89f004675703125792aba628e642d3ae4
Davyd Kohut <davyd.kohut@gmail.com>
2023-04-14 11:11:21 +0300

    0.10.38

--- src/interpreter/cells/portal/proxyFunctionCell.js
+++ src/interpreter/cells/portal/proxyFunctionCell.js
@@ -31,12 +31,36 @@ class ProxyFunctionCell extends Cell {
    */
   doCall(context, args, options = {}) {
     if (this.meta.isWrapped) {
-      args = Object.values(args).map((arg) => arg.asJsValue(context));
+      if (Array.isArray(args)) {
+        args = Object.values(args).map((arg) => arg.asJsValue(context));
+      } else {
+        const newArgs = {};
+        Object.entries(args).forEach(([name, arg]) => {
+          newArgs[name] = arg.asJsValue(context);
+        });
+        args = newArgs;
+      }
     }
+
+    const getArgument = (position, name) => {
+      if (Array.isArray(args)) {
+        if (args[position]) {
+          return args[position];
+        }
+
+        return this.meta.isWrapped ? null : this.mavka.empty;
+      } else {
+        if (name in args) {
+          return args[name];
+        }
+
+        return this.meta.isWrapped ? null : this.mavka.empty;
+      }
+    };
 
     if (this.meta.isAsync) {
       return new this.mavka.AsyncCell(this.mavka, async () => {
-        let result = await this.meta.fn(args, context, options);
+        let result = await this.meta.fn(args, context, { ...options, arg: getArgument });
 
         if (this.meta.isWrapped) {
           result = this.mavka.toCell(result);
@@ -45,7 +69,7 @@ class ProxyFunctionCell extends Cell {
         return result;
       });
     } else {
-      let result = this.meta.fn(args, context, options);
+      let result = this.meta.fn(args, context, { ...options, arg: getArgument });
 
       if (this.meta.isWrapped) {
         result = this.mavka.toCell(result);
---
+++ src/library/mathModule.js
@@ -0,0 +1,63 @@
+const makeMathModule = (mavka) => {
+  return mavka.makeModule("М", {
+    "Е": mavka.makeNumber(Math.E),
+    "ЛОГ10": mavka.makeNumber(Math.LN10),
+    "ЛОГ2": mavka.makeNumber(Math.LN2),
+    "ЛОГ2Е": mavka.makeNumber(Math.LOG2E),
+    "ЛОГ10Е": mavka.makeNumber(Math.LOG10E),
+    "ПІ": mavka.makeNumber(Math.PI),
+    "КОРІНЬ1_2": mavka.makeNumber(Math.SQRT1_2),
+    "КОРІНЬ2": mavka.makeNumber(Math.SQRT2),
+
+    "абс": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.abs(arg(0, "значення").asJsValue(context)))
+    ),
+    "арккос": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.acos(arg(0, "значення").asJsValue(context)))
+    ),
+    "арксин": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.asin(arg(0, "значення").asJsValue(context)))
+    ),
+    "арктан": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.atan(arg(0, "значення").asJsValue(context)))
+    ),
+    "арктан2": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.atan2(arg(0, "х").asJsValue(context), arg(0, "y").asJsValue(context)))
+    ),
+    "небо": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.ceil(arg(0, "значення").asJsValue(context)))
+    ),
+    "кос": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.cos(arg(0, "значення").asJsValue(context)))
+    ),
+    "експ": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.exp(arg(0, "значення").asJsValue(context)))
+    ),
+    "земля": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.floor(arg(0, "значення").asJsValue(context)))
+    ),
+    "найбільше": mavka.makeProxyFunction((args, context) => mavka.makeNumber(
+      Math.max(...Object.values(args).map((arg) => arg.asJsValue(context))))
+    ),
+    "найменше": mavka.makeProxyFunction((args, context) => mavka.makeNumber(
+      Math.max(...Object.values(args).map((arg) => arg.asJsValue(context))))
+    ),
+    "степінь": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.pow(arg(0, "а").asJsValue(context), arg(1, "б").asJsValue(context)))
+    ),
+    "округлити": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.round(arg(0, "значення").asJsValue(context)))
+    ),
+    "син": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.sin(arg(0, "значення").asJsValue(context)))
+    ),
+    "корінь": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.sqrt(arg(0, "значення").asJsValue(context)))
+    ),
+    "тан": mavka.makeProxyFunction((args, context, { arg }) => mavka.makeNumber(
+      Math.tan(arg(0, "значення").asJsValue(context)))
+    )
+  });
+};
+
+export default makeMathModule;
