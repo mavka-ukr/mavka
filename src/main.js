0d899eec111bcd869a16cac4abc5ef152e18bc49
Davyd Kohut <davyd.kohut@gmail.com>
2023-11-28 02:47:59 +0200

    0.50.35

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ import TernaryNode from "mavka-parser/src/ast/TernaryNode.js";
 let DEBUG_ID = 0;
 
 class Mavka {
-  static VERSION = "0.50.34";
+  static VERSION = "0.50.35";
 
   constructor(options = {}) {
     this.debugInfoVarNames = new Map();
