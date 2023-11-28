e5b47e793c3f168a7a5e64c5e0487670a604df00
Davyd Kohut <davyd.kohut@gmail.com>
2023-11-28 11:32:22 +0200

    0.50.42

--- src/bin/pak.js
+++ src/bin/pak.js
@@ -1,33 +1,3 @@
 #!/usr/bin/env node
 
-import { pakAddFromRepoUrl } from "mavka-pak";
-
-const command = process.argv[2];
-
-if (command === "додати") {
-  const name = process.argv[3];
-  const repoUrl = process.argv[4];
-
-  if (repoUrl) {
-    console.log(`Додаємо "${name}" з ${repoUrl}...`);
-
-    await pakAddFromRepoUrl(name, repoUrl, process.cwd());
-
-    console.log(`"${name}" додано.`);
-  } else {
-    console.log("пак додати <назва> <репозиторій>");
-  }
-} else if (command === "видалити") {
-  const name = process.argv[3];
-
-  console.log("пак видалити <назва>");
-} else {
-  console.log(`
-Використання:
-  пак <команда> [...аргументи]
-
-Доступні команди:
-  пак додати <назва> <репозиторій> — додає пак з git-репозиторію
-  пак видалити <назва> — видаляє пак
-  `.trim());
-}
+console.log("Зовсім не скоро...");
--- src/lib/std.js
+++ src/lib/std.js
@@ -1,14 +1,22 @@
 // std
-var $друк = function(params, di) {
-  console.log(...params.map((v) => mavka_to_pretty_string(v)));
+var $друк = function(args, di) {
+  console.log(...args.map((v) => mavka_to_pretty_string(v)));
+};
+
+var $вивести = function(args, di) {
+  process.stdout.write(args.map((v) => mavka_to_pretty_string(v)).join(""));
 };
 
 // deprecated. use 0..n instead
-var $діапазон = function* (params, di) {
-  const start = Array.isArray(params) ? params[0] : params.старт;
-  const end = Array.isArray(params) ? params[1] : params.стоп;
-
+var $діапазон = function* (args, di) {
+  const start = Array.isArray(args) ? args[0] : args.старт;
+  const end = Array.isArray(args) ? args[1] : args.стоп;
   for (let i = start; i < end; i++) {
     yield i;
   }
+};
+
+var $читати = function(args, di) {
+  var ask = mavka_arg(args, 0, "питання", $текст, "", di);
+  return mavka_read({ sigint: true, encoding: "windows-1251" })(ask);
 };
