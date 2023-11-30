7d1110005d53fb003db93d34f104d9d31392c5c1
Davyd Kohut <davyd.kohut@gmail.com>
2023-11-30 07:59:13 +0200

    0.51.9

--- src/main.js
+++ src/main.js
@@ -100,7 +100,7 @@ import { DiiaParserSyntaxError } from "mavka-parser/src/utils/errors.js";
 let DEBUG_ID = 0;
 
 class Mavka {
-  static VERSION = "0.51.8";
+  static VERSION = "0.51.9";
 
   constructor(options = {}) {
     this.debugInfoVarNames = new Map();
