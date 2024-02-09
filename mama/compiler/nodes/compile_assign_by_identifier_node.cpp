333bd8049c0b96090c8aed1aacbac136909cbe59
Davyd Kohut <davyd.kohut@gmail.com>
2024-02-09 19:28:42 +0200

    🧬

--- mama/compiler/nodes/compile_assign_by_identifier_node.cpp
+++ mama/compiler/nodes/compile_assign_by_identifier_node.cpp
@@ -4,21 +4,22 @@ namespace mavka::mama {
   MaCompilationResult compile_assign_by_identifier_node(
       MaMa* M,
       mavka::ast::AssignByIdentifierNode* assign_by_identifier_node) {
-    // if (assign_by_identifier_node->op == "=") {
-    //   const auto value_result =
-    //       compile_node(M, assign_by_identifier_node->value);
-    //   if (value_result->error) {
-    //     return value_result;
-    //   }
-    //   const auto result = compile_node(M, assign_by_identifier_node->left);
-    //   if (result->error) {
-    //     return result;
-    //   }
-    //   M->instructions.push_back(
-    //       new MaInstruction(OP_SET, 0,
-    //       assign_by_identifier_node->identifier));
-    //   return success();
-    // }
+    if (assign_by_identifier_node->op == "=") {
+      const auto value_result =
+          compile_node(M, assign_by_identifier_node->value);
+      if (value_result.error) {
+        return value_result;
+      }
+      const auto result = compile_node(M, assign_by_identifier_node->left);
+      if (result.error) {
+        return result;
+      }
+      M->code.push_back(
+          MaInstruction{OP_SET,
+                        {.set = new MaSetInstructionArgs(
+                             assign_by_identifier_node->identifier)}});
+      return success();
+    }
     return error(mavka::ast::make_ast_some(assign_by_identifier_node),
                  "Not implemented");
   }
