dfdb3514767d58558e067f3e9f97cc5495c3e461
Davyd Kohut <davyd.kohut@gmail.com>
2024-02-10 10:35:57 +0200

    🧬

--- mama/compiler/nodes/compile_comparison_node.cpp
+++ mama/compiler/nodes/compile_comparison_node.cpp
@@ -3,7 +3,7 @@
 namespace mavka::mama {
   MaCompilationResult compile_comparison_node(
       MaMa* M,
-      const mavka::ast::ComparisonNode* comparison_node) {
+      mavka::ast::ComparisonNode* comparison_node) {
     const auto left = compile_node(M, comparison_node->left);
     if (left.error) {
       return left;
@@ -15,11 +15,25 @@ namespace mavka::mama {
     if (comparison_node->op == "==" || comparison_node->op == "рівно") {
       M->code.push_back(MaInstruction{OP_EQ});
     } else if (comparison_node->op == "!=" ||
-               comparison_node->op == "не рівно") {
+               comparison_node->op == "не рівно" ||
+               comparison_node->op == "нерівно") {
       M->code.push_back(MaInstruction{OP_EQ});
       M->code.push_back(MaInstruction{OP_NOT});
     } else if (comparison_node->op == ">" || comparison_node->op == "більше") {
       M->code.push_back(MaInstruction{OP_GT});
+    } else if (comparison_node->op == ">=" ||
+               comparison_node->op == "не менше" ||
+               comparison_node->op == "неменше") {
+      M->code.push_back(MaInstruction{OP_GE});
+    } else if (comparison_node->op == "<" || comparison_node->op == "менше") {
+      M->code.push_back(MaInstruction{OP_LT});
+    } else if (comparison_node->op == "<=" ||
+               comparison_node->op == "не більше" ||
+               comparison_node->op == "небільше") {
+      M->code.push_back(MaInstruction{OP_LE});
+    } else {
+      return error(mavka::ast::make_ast_some(comparison_node),
+                   "Невідомий оператор порівняння: " + comparison_node->op);
     }
     return success();
   }
