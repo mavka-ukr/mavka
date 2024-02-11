1b3648b0daa22d69b051cbd0e2ad515525b9dc65
Davyd Kohut <davyd.kohut@gmail.com>
2024-02-11 18:11:46 +0200

    🧬

--- dist.sh
+++ dist.sh
@@ -20,5 +20,5 @@ mkdir -p мавка-"$VERSION"
 cp ../build-Obin/мавка_оптимізовано мавка-"$VERSION"/мавка
 cp ../LICENSE мавка-"$VERSION"/Ліцензія
 echo -en "Вітання!\n\nВи завантажили Мавку $VERSION.\nЦя версія є експериментальною і може містити вади.\nЯкщо ви знайшли таку ваду — зробіть issue на GitHub-і: https://github.com/mavka-ukr/mavka\n\nДокументація до Мавки знаходиться тут: https://мавка.укр/документація" >мавка-"$VERSION"/Прочитайка
-zip -r -9 мавка-"$VERSION"-linux_64.zip мавка-"$VERSION"
+zip -r -9 mavka-"$VERSION"-linux_64.zip мавка-"$VERSION"
 cd ..
