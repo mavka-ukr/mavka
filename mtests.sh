6bb34947cbbb8f8c7565a7b599e5ee807fe359de
Davyd Kohut <davyd.kohut@gmail.com>
2024-02-13 11:37:16 +0200

    🧬

--- mtests.sh
+++ mtests.sh
@@ -5,12 +5,12 @@ MAVKA_VERSION=$(cat VERSION)
 
 for mtest in ${MTESTS[@]}; do
   if [[ $mtest == *"_тест.м" ]]; then
-    echo -en "Тестуємо ./mtests/$mtest"
+    echo -en "$mtest"
     result=$("./dist/мавка-$MAVKA_VERSION/мавка" "./mtests/$mtest")
     if [ "$result" == "ок" ]; then
         echo -en ": \e[32mok\e[0m\n"
     else
-        echo -en "\n\e[31m$result\e[0m\n"
+        echo -en ":\n\e[31m$result\e[0m\n"
     fi
   fi
 done
