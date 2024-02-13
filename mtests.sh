f7a9bcd1e3e930d6c32a24382bd44f6bde1c806c
Davyd Kohut <davyd.kohut@gmail.com>
2024-02-13 11:23:06 +0200

    🧬

--- mtests.sh
+++ mtests.sh
@@ -4,6 +4,7 @@ MTESTS=$(ls mtests)
 MAVKA_VERSION=$(cat VERSION)
 
 for mtest in ${MTESTS[@]}; do
+  if [[ $mtest == *"_тест.м" ]]; then
     echo -en "Тестуємо ./mtests/$mtest"
     result=$("./dist/мавка-$MAVKA_VERSION/мавка" "./mtests/$mtest")
     if [ "$result" == "ок" ]; then
@@ -11,4 +12,5 @@ for mtest in ${MTESTS[@]}; do
     else
         echo -en "\n\e[31m$result\e[0m\n"
     fi
+  fi
 done
