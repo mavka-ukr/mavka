04d75fcf7d55646a45920b28b8426907c91e310c
Davyd Kohut <davyd.kohut@gmail.com>
2024-02-29 01:22:30 +0200

    🧬

--- mama/compiler/nodes/compile_wait_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "../../mama.h"
-
-namespace mavka::mama {
-  MaCompilationResult compile_wait_node(MaMa* M,
-                                        MaCode* code,
-                                        mavka::ast::ASTValue* ast_value) {
-    return error(ast_value, "Вказівка \"чекати\" тимчасово недоступна.");
-  }
-} // namespace mavka::mama
