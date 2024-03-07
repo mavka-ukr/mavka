ca7d0f7200a7df2ab17e977b5be575cfd81d3725
Davyd Kohut <davyd.kohut@gmail.com>
2024-03-07 11:08:51 +0200

    🧬

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ if [ "$TARGET" = "" ]; then
     exit 1
 fi
 
-LIBS_BUNDLE_HEAD="std::unordered_map<std::string, std::string> lib_modules = {"
+LIBS_BUNDLE_HEAD="std::unordered_map<std::string, std::string> MAVKA_LIB_MODULES = {"
 LIBS_BUNDLE_TAIL="};"
 LIBS=$(find "біб" | grep --color=never ".м\$")
 LIBS_BUNDLE_CONTENT=""
@@ -21,7 +21,7 @@ for LIB in $LIBS; do
 }()},"
 done
 LIBS_BUNDLE_CONTENT="$LIBS_BUNDLE_HEAD $LIBS_BUNDLE_CONTENT $LIBS_BUNDLE_TAIL"
-echo "$LIBS_BUNDLE_CONTENT" >lib_bundle.h
+echo "$LIBS_BUNDLE_CONTENT" >MAVKA_LIB_MODULES.h
 
 if [ "$TARGET" = "bin" ] || [ "$TARGET" = "all" ]; then
     mkdir -p build-bin
---
+++ src/take.cpp
@@ -0,0 +1,81 @@
+#include "mavka.h"
+
+namespace mavka {
+#include "../MAVKA_LIB_MODULES.h"
+
+  MaCell TakeBib(MaMa* M,
+                 const std::vector<std::string>& parts,
+                 const MaLocation& location) {
+    const auto full_path =
+        "біб/" + mavka::internal::tools::implode(parts, "/") + ".м";
+    if (M->loaded_file_modules.contains(full_path)) {
+      return MaCell::Object(M->loaded_file_modules[full_path]);
+    }
+    if (full_path == "біб/читати.м") {
+      const auto read_module_object = BibInitReadModule(M);
+      M->loaded_file_modules[full_path] = read_module_object;
+      return MaCell::Object(read_module_object);
+    }
+    if (MAVKA_LIB_MODULES.contains(full_path)) {
+      const auto& name = parts.back();
+      return M->DoTake(full_path, name, MAVKA_LIB_MODULES[full_path], location);
+    }
+    return MaCell::Error(MaError::Create(
+        M, "Модуль \"" + full_path + "\" не знайдено в бібліотеці.", location));
+  }
+
+  MaCell TakePath(MaMa* M,
+                  const std::string& raw_path,
+                  const MaLocation& location) {
+    const auto canonical_path = std::filesystem::weakly_canonical(raw_path);
+    const auto path = canonical_path.string();
+    if (!std::filesystem::exists(canonical_path)) {
+      return MaCell::Error(MaError::Create(
+          M, "Шлях \"" + canonical_path.string() + "\" не існує.", location));
+    }
+    if (!std::filesystem::is_regular_file(canonical_path)) {
+      return MaCell::Error(MaError::Create(
+          M, "Шлях \"" + path + "\" не вказує на файл.", location));
+    }
+
+    if (M->loaded_file_modules.contains(path)) {
+      return MaCell::Object(M->loaded_file_modules[path]);
+    }
+
+    auto file = std::ifstream(path);
+    if (!file.is_open()) {
+      return MaCell::Error(MaError::Create(
+          M, "Не вдалося прочитати файл \"" + path + "\".", location));
+    }
+
+    const auto fs_path = std::filesystem::path(path);
+    const auto name = fs_path.stem().string();
+
+    const auto source = std::string(std::istreambuf_iterator(file),
+                                    std::istreambuf_iterator<char>());
+
+    return M->DoTake(path, name, source, location);
+  }
+
+  MaCell TakeFn(MaMa* M,
+                const std::string& repository,
+                bool relative,
+                const std::vector<std::string>& parts,
+                const MaLocation& location) {
+    if (repository == "біб") {
+      return TakeBib(M, parts, location);
+    }
+    if (!repository.empty()) {
+      return MaCell::Error(MaError::Create(
+          M, "Не підтримується взяття з репозиторію.", location));
+    }
+    if (relative) {
+      return MaCell::Error(MaError::Create(
+          M, "Не підтримується взяття відносного шляху.", location));
+    }
+    const auto cwd = std::filesystem::current_path();
+    const auto raw_path =
+        cwd.string() + "/" + mavka::internal::tools::implode(parts, "/") + ".м";
+    return TakePath(M, raw_path, location);
+  }
+} // namespace mavka
