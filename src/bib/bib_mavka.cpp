e99d0f25ff92d5d5c4763fc960813aa9fa39d988
Davyd Kohut <davyd.kohut@gmail.com>
2024-03-08 11:06:00 +0200

    🧬

--- src/bib/bib_mavka.cpp
+++ src/bib/bib_mavka.cpp
@@ -1,3 +1,5 @@
+#include <dlfcn.h>
+#include "../../include/mavka_api_v0.h"
 #include "../mavka.h"
 
 namespace mavka {
@@ -13,14 +15,51 @@ namespace mavka {
         MaError::Create(M, "Очікується що код буде текстом.", location));
   }
 
+  MaCell BibMavkaExtendNativeFn(MaMa* M,
+                                MaObject* o,
+                                MaArgs* args,
+                                const MaLocation& location) {
+    const auto version_cell = args->Get(0, "версія");
+    if (!version_cell.IsNumber()) {
+      return MaCell::Error(
+          MaError::Create(M, "Очікується що версія буде числом.", location));
+    }
+    const auto path_cell = args->Get(1, "шлях");
+    if (!(path_cell.IsObject() && path_cell.IsObjectText())) {
+      return MaCell::Error(
+          MaError::Create(M, "Очікується що шлях буде текстом.", location));
+    }
+
+    void* dobject = dlopen(path_cell.AsText()->data.c_str(), RTLD_LAZY);
+    if (!dobject) {
+      return MaCell::Error(
+          MaError::Create(M, std::string(dlerror()), location));
+    }
+    dlerror();
+
+    void* extfptr = dlsym(dobject, "мавка_розширити_модуль");
+    if (extfptr == nullptr) {
+      return MaCell::Error(
+          MaError::Create(M, std::string(dlerror()), location));
+    }
+
+    double (*load_extension)(mavka::api::v0::MavkaOptions mavkaOptions);
+    const auto f = reinterpret_cast<decltype(load_extension)>(extfptr);
+    f({.version = (char*)MAVKA_VERSION});
+
+    return MaCell::Empty();
+  }
+
   // взяти біб мавка
   MaObject* BibInitMavkaModule(MaMa* M) {
-    const auto mavka_module_object = MaModule::Create(M, "мавка");
-    mavka_module_object->SetProperty("версія",
-                                     MaText::Create(M, MAVKA_VERSION));
-    mavka_module_object->SetProperty(
+    const auto mavka_module_o = MaModule::Create(M, "мавка");
+    mavka_module_o->SetProperty("версія", MaText::Create(M, MAVKA_VERSION));
+    mavka_module_o->SetProperty(
         "виконати",
         MaNative::Create(M, "виконати", BibMavkaEvalNativeFn, nullptr));
-    return mavka_module_object;
+    mavka_module_o->SetProperty(
+        "розширити",
+        MaNative::Create(M, "розширити", BibMavkaExtendNativeFn, nullptr));
+    return mavka_module_o;
   }
 } // namespace mavka
