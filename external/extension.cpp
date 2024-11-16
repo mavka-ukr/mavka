00be101c1f414a33928e054fb207ef6f4dc76356
Davyd Kohut <davyd.kohut@gmail.com>
2024-11-16 12:12:00 +0200

    🧬

--- external/extension.cpp
+++ external/extension.cpp
@@ -4,6 +4,10 @@
 #include <cstring>
 #include <locale>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 extern "C" unsigned char* прочитати_файл(unsigned char* шлях) {
   FILE* file = fopen((char*)шлях, "rb");
   if (file == nullptr) {
@@ -19,9 +23,40 @@ extern "C" unsigned char* прочитати_файл(unsigned char* шлях) {
   return buffer;
 }
 
-extern "C" unsigned char* перетворити_ю32_в_ю8(char32_t* значення) {
+extern "C" unsigned char* mavka_convert_utf32_to_utf8(char32_t* value) {
   std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> utf32conv;
-  std::u32string utf32 = значення;
+  std::u32string utf32 = value;
   return reinterpret_cast<unsigned char*>(
       strdup(utf32conv.to_bytes(utf32).c_str()));
+}
+
+extern "C" void mavka_print_utf8(unsigned char* value) {
+#ifdef _WIN32
+  auto len = strlen((char*)value);
+  auto value16 = (unsigned short int*)malloc((len + 1) * 2);
+  // encode to utf16
+  auto outlen = MultiByteToWideChar(CP_UTF8, 0, (char*)value, len,
+                                    (wchar_t*)value16, len);
+  value16[outlen] = 0;
+  WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), value16,
+                wcslen((wchar_t*)value16), NULL, NULL);
+#else
+  printf("%s", (char*)value);
+#endif
+}
+
+extern "C" void mavka_println_utf8(unsigned char* value) {
+#ifdef _WIN32
+  auto len = strlen((char*)value);
+  auto value16 = (unsigned short int*)malloc((len + 1) * 2);
+  // encode to utf16
+  auto outlen = MultiByteToWideChar(CP_UTF8, 0, (char*)value, len,
+                                    (wchar_t*)value16, len);
+  value16[outlen] = 0;
+  WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), value16,
+                wcslen((wchar_t*)value16), NULL, NULL);
+  WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), L"\n", 1, NULL, NULL);
+#else
+  printf("%s\n", (char*)value);
+#endif
 }
--- external/main.cpp
+++ external/main.cpp
@@ -6,22 +6,25 @@ extern "C" int стартувати_мавку(int argc, unsigned char** argv);
 #ifdef _WIN32
 #include <windows.h>
 
-int wmain(int argc, wchar_t** argv) {
-  int argc8 = 0;
-  unsigned char** argv8 = new unsigned char*[argc];
-  for (int i = 0; i < argc; i++) {
-    int len = WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, NULL, 0, NULL, NULL);
-    argv8[i] = new unsigned char[len];
-    WideCharToMultiByte(CP_UTF8, 0, argv[i], -1,
-                        reinterpret_cast<char*>(argv8[i]), len, NULL, NULL);
-    argc8++;
-  }
-  int ret = стартувати_мавку(argc8, argv8);
-  for (int i = 0; i < argc; i++) {
-    delete[] argv8[i];
-  }
-  delete[] argv8;
-  return ret;
+//int wmain(int argc, wchar_t** argv) {
+//  int argc8 = 0;
+//  unsigned char** argv8 = new unsigned char*[argc];
+//  for (int i = 0; i < argc; i++) {
+//    int len = WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, NULL, 0, NULL, NULL);
+//    argv8[i] = new unsigned char[len];
+//    WideCharToMultiByte(CP_UTF8, 0, argv[i], -1,
+//                        reinterpret_cast<char*>(argv8[i]), len, NULL, NULL);
+//    argc8++;
+//  }
+//  int ret = стартувати_мавку(argc8, argv8);
+//  for (int i = 0; i < argc; i++) {
+//    delete[] argv8[i];
+//  }
+//  delete[] argv8;
+//  return ret;
+//}
+int main(int argc, char** argv) {
+  return стартувати_мавку(argc, reinterpret_cast<unsigned char**>(argv));
 }
 #endif
 
