1d841270ca59a0089821ba611209d29ea05237f8
Davyd Kohut <davyd.kohut@gmail.com>
2024-11-17 17:02:37 +0200

    🧬

--- external/extension.cpp
+++ external/extension.cpp
@@ -5,10 +5,6 @@
 #include <filesystem>
 #include <locale>
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
 extern "C" unsigned char* прочитати_файл(unsigned char* шлях) {
   FILE* file = fopen((char*)шлях, "rb");
   if (file == nullptr) {
@@ -32,34 +28,11 @@ extern "C" unsigned char* mama_convert_utf32_to_utf8(char32_t* value) {
 }
 
 extern "C" void mama_print_utf8(unsigned char* value) {
-#ifdef _WIN32
-  auto len = strlen((char*)value);
-  auto value16 = (unsigned short int*)malloc((len + 1) * 2);
-  // encode to utf16
-  auto outlen = MultiByteToWideChar(CP_UTF8, 0, (char*)value, len,
-                                    (wchar_t*)value16, len);
-  value16[outlen] = 0;
-  WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), value16,
-                wcslen((wchar_t*)value16), NULL, NULL);
-#else
   printf("%s", (char*)value);
-#endif
 }
 
 extern "C" void mama_println_utf8(unsigned char* value) {
-#ifdef _WIN32
-  auto len = strlen((char*)value);
-  auto value16 = (unsigned short int*)malloc((len + 1) * 2);
-  // encode to utf16
-  auto outlen = MultiByteToWideChar(CP_UTF8, 0, (char*)value, len,
-                                    (wchar_t*)value16, len);
-  value16[outlen] = 0;
-  WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), value16,
-                wcslen((wchar_t*)value16), NULL, NULL);
-  WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), L"\n", 1, NULL, NULL);
-#else
   printf("%s\n", (char*)value);
-#endif
 }
 
 extern "C" void виправити_шлях(unsigned char* шлях, unsigned char** вихід) {
