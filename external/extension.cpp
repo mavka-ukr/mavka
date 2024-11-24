da182654b06662550ed8a87fc8f720140ef53e40
Davyd Kohut <davyd.kohut@gmail.com>
2024-11-24 18:11:11 +0200

    🧬

--- external/extension.cpp
+++ external/extension.cpp
@@ -90,3 +90,29 @@ extern "C" unsigned long mama_bitnot(unsigned long value) {
 extern "C" double mama_negate(double value) {
   return -value;
 }
+
+extern "C" {
+#include <locale.h>
+#include <stdlib.h>
+#include "isocline/include/isocline.h"
+
+static void completer(ic_completion_env_t* cenv, const char* input) {}
+
+static void highlighter(ic_highlight_env_t* henv,
+                        const char* input,
+                        void* arg) {}
+
+void mavka_dialog(void* data, void (*run)(void* data, unsigned char* value)) {
+  setlocale(LC_ALL, "C.UTF-8");
+  ic_set_default_completer(&completer, nullptr);
+  ic_set_default_highlighter(highlighter, nullptr);
+  ic_enable_auto_tab(true);
+  ic_set_prompt_marker(" ", nullptr);
+  ic_set_history(nullptr, 200);
+  unsigned char* input;
+  while ((input = (unsigned char*)ic_readline("\e[0m-")) != nullptr) {
+    run(data, input);
+    free(input);
+  }
+}
+}
--- скрипти/збудувати.sh
+++ скрипти/збудувати.sh
@@ -16,6 +16,12 @@ cmake ../../external -G Ninja -DCMAKE_CXX_FLAGS=-fdiagnostics-color=always
 ninja
 cd -
 
+mkdir -p "build-$PLATFORM/external/isocline"
+cd "build-$PLATFORM/external/isocline"
+cmake "../../../external/isocline" -G Ninja
+ninja
+cd -
+
 ціль .плавлення/мавка/бібліотека/М.ll скомпілювати мавка/бібліотека/М.ц
 ціль .плавлення/мавка/бібліотека/мавка.ll скомпілювати мавка/бібліотека/мавка.ц
 ціль .плавлення/мавка/бібліотека/МаМа.ll скомпілювати мавка/бібліотека/МаМа.ц
@@ -28,7 +34,7 @@ cd -
 
 ціль .плавлення/старт.ll скомпілювати старт.ц
 
-$CXX -static -O3 -o "$OUT" \
+$CXX -O3 -o "$OUT" \
   .плавлення/мавка/бібліотека/М.ll \
   .плавлення/мавка/бібліотека/мавка.ll \
   .плавлення/мавка/бібліотека/МаМа.ll \
@@ -39,4 +45,5 @@ $CXX -static -O3 -o "$OUT" \
   build-"$PLATFORM"/external/libmavka_external.a \
   build-"$PLATFORM"/external/mavka_parser/libmavka_parser.a \
   build-"$PLATFORM"/external/mavka_parser/syntax/libmavka_syntax.a \
-  build-"$PLATFORM"/external/mavka_parser/syntax/antlr4-cpp-runtime/libantlr4_cpp_runtime.a
+  build-"$PLATFORM"/external/mavka_parser/syntax/antlr4-cpp-runtime/libantlr4_cpp_runtime.a \
+  build-"$PLATFORM"/external/isocline/libisocline.a
