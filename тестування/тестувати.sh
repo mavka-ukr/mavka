74e5365accfdb601e3627facbd244daa49d4e8e5
Davyd Kohut <davyd.kohut@gmail.com>
2024-11-28 21:54:04 +0200

    🧬

--- тестування/тестувати.sh
+++ тестування/тестувати.sh
@@ -72,16 +72,34 @@ TESTS=(
   "тести/Модуль/0_модуль_працює.м"
 )
 
+SUCCESS_COUNT=0
+FAILED_COUNT=0
+
 for test in ${TESTS[@]}; do
   echo -en "$test"
   result=$($MAVKA "$test")
   if [ $? -ne 0 ]; then
     echo -en ":\n\e[31m$result\e[0m\n"
+    FAILED_COUNT=$((FAILED_COUNT+1))
   elif [ "$result" == "" ]; then
     echo -en ": \e[32mпройдено\e[0m\n"
+    SUCCESS_COUNT=$((SUCCESS_COUNT+1))
   else
     echo -en ":\n\e[31m$result\e[0m\n"
+    FAILED_COUNT=$((FAILED_COUNT+1))
   fi
 done
+
+echo -e "---"
+if [ $SUCCESS_COUNT -eq 0 ]; then
+  echo -e "Пройдено: $SUCCESS_COUNT"
+else
+  echo -e "Пройдено: \e[32m$SUCCESS_COUNT\e[0m"
+fi
+if [ $FAILED_COUNT -eq 0 ]; then
+  echo -e "Провалено: \e[32m$FAILED_COUNT\e[0m"
+else
+  echo -e "Провалено: \e[31m$FAILED_COUNT\e[0m"
+fi
 
 cd - >/dev/null
