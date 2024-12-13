f4e85c387c8bb622a151612781d9b4328f034ff5
Davyd Kohut <davyd.kohut@gmail.com>
2024-12-13 11:17:24 +0200

    деякі покращення

--- external/main.cpp
+++ external/main.cpp
@@ -1,6 +1,3 @@
-// наразі змушені використовувати точку входу з C++
-// так як Ціль ще не дуже може зробити те що треба на Windows
-
 extern "C" int стартувати_мавку(int argc, unsigned char** argv);
 
 int main(int argc, char** argv) {
