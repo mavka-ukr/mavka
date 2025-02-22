7cde69b5903ec8b308507f3ff6c9438404487465
Davyd Kohut <davyd.kohut@gmail.com>
2025-02-23 00:38:21 +0200

    почато перехід на власний розбирач на заміну ANTLR4

--- external/extension.cpp
+++ external/extension.cpp
@@ -10,13 +10,12 @@
 #include <iostream>
 #include <locale>
 
-extern "C" char* mavka_read_file(char* шлях,
-                                 size_t розмір_шляху,
-                                 size_t* buffer_size) {
-  char* path = (char*)malloc(розмір_шляху + 1);
+extern "C" char *mavka_read_file(char *шлях, size_t розмір_шляху,
+                                 size_t *buffer_size) {
+  char *path = (char *)malloc(розмір_шляху + 1);
   memcpy(path, шлях, розмір_шляху);
   path[розмір_шляху] = 0;
-  FILE* file = fopen(path, "rb");
+  FILE *file = fopen(path, "rb");
   free(path);
   if (file == nullptr) {
     return nullptr;
@@ -24,7 +23,7 @@ extern "C" char* mavka_read_file(char* шлях,
   fseek(file, 0, SEEK_END);
   auto length = ftell(file);
   fseek(file, 0, SEEK_SET);
-  auto buffer = (char*)malloc(length + 1);
+  auto buffer = (char *)malloc(length + 1);
   fread(buffer, 1, length, file);
   fclose(file);
   buffer[length] = 0;
@@ -32,33 +31,31 @@ extern "C" char* mavka_read_file(char* шлях,
   return buffer;
 }
 
-extern "C" char* mavka_fix_path(char* шлях,
-                                size_t розмір_шляху,
-                                size_t* buffer_size) {
+extern "C" char *mavka_fix_path(char *шлях, size_t розмір_шляху,
+                                size_t *buffer_size) {
   std::string path(шлях, розмір_шляху);
   std::filesystem::path p(path);
-  char* fixed_path = strdup(absolute(weakly_canonical(p)).string().c_str());
+  char *fixed_path = strdup(absolute(weakly_canonical(p)).string().c_str());
   *buffer_size = strlen(fixed_path);
   return fixed_path;
 }
 
-extern "C" unsigned char mavka_check_if_str_ends_with(unsigned char* value,
-                                                      unsigned char* suffix) {
-  std::string str = (char*)value;
-  std::string suf = (char*)suffix;
+extern "C" unsigned char mavka_check_if_str_ends_with(unsigned char *value,
+                                                      unsigned char *suffix) {
+  std::string str = (char *)value;
+  std::string suf = (char *)suffix;
   return str.ends_with(suf);
 }
 
-extern "C" void mavka_get_filename_without_extension(unsigned char* шлях,
-                                                     unsigned char** вихід) {
-  std::string path = (char*)шлях;
+extern "C" void mavka_get_filename_without_extension(unsigned char *шлях,
+                                                     unsigned char **вихід) {
+  std::string path = (char *)шлях;
   std::filesystem::path p(path);
-  *вихід = (unsigned char*)strdup(p.stem().string().c_str());
+  *вихід = (unsigned char *)strdup(p.stem().string().c_str());
 }
 
-extern "C" uint64_t mavka_read_from_stdin(char* prefix,
-                                          size_t prefix_size,
-                                          char** output) {
+extern "C" uint64_t mavka_read_from_stdin(char *prefix, size_t prefix_size,
+                                          char **output) {
   std::cout << std::string(prefix, prefix_size);
   std::string line;
   if (std::cin.eof()) {
@@ -74,13 +71,14 @@ extern "C" {
 #if MAVKA_READLINE == 1
 #include <readline/readline.h>
 
-char* _mavka_readline(char* prefix) {
-  return readline(prefix);
-}
+char *_mavka_readline(char *prefix) { return readline(prefix); }
 #else
-char* _mavka_readline(char* prefix) {
+char *_mavka_readline(char *prefix) {
   std::cout << prefix;
   std::string line;
+  if (std::cin.eof()) {
+    return nullptr;
+  }
   std::getline(std::cin, line);
   return strdup(line.c_str());
 }
@@ -90,40 +88,44 @@ void _mavka_readline_init() {
   //
 }
 
-void mavka_dialog(void* data,
-                  void (*run)(void* data, char* value, size_t value_size)) {
+void mavka_dialog(void *data,
+                  void (*run)(void *data, char *value, size_t value_size)) {
   _mavka_readline_init();
-  char* input;
-  char* prefix = strdup("- ");
-  while ((input = _mavka_readline(prefix)) != nullptr) {
-    run(data, input, strlen(input));
-    free(input);
+  char *input;
+  char *prefix = strdup("- ");
+read:
+  input = _mavka_readline(prefix);
+  if (input == nullptr) {
+    goto out;
   }
+  run(data, input, strlen(input));
+  free(input);
+  goto read;
+out:
   free(prefix);
 }
 }
 
-extern "C" size_t mavka_get_path_directory(unsigned char* path,
-                                           unsigned char** output) {
-  std::string str = (char*)path;
+extern "C" size_t mavka_get_path_directory(unsigned char *path,
+                                           unsigned char **output) {
+  std::string str = (char *)path;
   std::filesystem::path p(str);
-  *output = (unsigned char*)strdup(p.parent_path().string().c_str());
-  return strlen((char*)*output);
+  *output = (unsigned char *)strdup(p.parent_path().string().c_str());
+  return strlen((char *)*output);
 }
 
-extern "C" void* mavka_load_shared_object_function_ptr_from_file(
-    char* path,
-    size_t path_size,
-    unsigned char* name) {
+extern "C" void *
+mavka_load_shared_object_function_ptr_from_file(char *path, size_t path_size,
+                                                unsigned char *name) {
 #if defined(__linux__)
-  void* dobject = dlopen(std::string(path, path_size).c_str(), RTLD_LAZY);
+  void *dobject = dlopen(std::string(path, path_size).c_str(), RTLD_LAZY);
   if (dobject == nullptr) {
     if (auto err = dlerror()) {
       std::cout << err << std::endl;
     }
     return nullptr;
   }
-  void* extfptr = dlsym(dobject, (char*)name);
+  void *extfptr = dlsym(dobject, (char *)name);
   if (extfptr == nullptr) {
     if (auto err = dlerror()) {
       std::cout << err << std::endl;
@@ -136,90 +138,56 @@ extern "C" void* mavka_load_shared_object_function_ptr_from_file(
 #endif
 }
 
-extern "C" double mavka_sin(double value) {
-  return sin(value);
-}
+extern "C" double mavka_sin(double value) { return sin(value); }
 
-extern "C" double mavka_cos(double value) {
-  return cos(value);
-}
+extern "C" double mavka_cos(double value) { return cos(value); }
 
-extern "C" double mavka_tan(double value) {
-  return tan(value);
-}
+extern "C" double mavka_tan(double value) { return tan(value); }
 
-extern "C" double mavka_asin(double value) {
-  return asin(value);
-}
+extern "C" double mavka_asin(double value) { return asin(value); }
 
-extern "C" double mavka_acos(double value) {
-  return acos(value);
-}
+extern "C" double mavka_acos(double value) { return acos(value); }
 
-extern "C" double mavka_atan(double value) {
-  return atan(value);
-}
+extern "C" double mavka_atan(double value) { return atan(value); }
 
-extern "C" double mavka_atan2(double y, double x) {
-  return atan2(y, x);
-}
+extern "C" double mavka_atan2(double y, double x) { return atan2(y, x); }
 
-extern "C" double mavka_abs(double value) {
-  return abs(value);
-}
+extern "C" double mavka_abs(double value) { return abs(value); }
 
-extern "C" double mavka_exp(double value) {
-  return exp(value);
-}
+extern "C" double mavka_exp(double value) { return exp(value); }
 
-extern "C" double mavka_sqrt(double value) {
-  return sqrt(value);
-}
+extern "C" double mavka_sqrt(double value) { return sqrt(value); }
 
-extern "C" double mavka_pow(double a, double b) {
-  return pow(a, b);
-}
+extern "C" double mavka_pow(double a, double b) { return pow(a, b); }
 
-extern "C" double mavka_ceil(double value) {
-  return ceil(value);
-}
+extern "C" double mavka_ceil(double value) { return ceil(value); }
 
-extern "C" double mavka_floor(double value) {
-  return floor(value);
-}
+extern "C" double mavka_floor(double value) { return floor(value); }
 
-extern "C" double mavka_round(double value) {
-  return round(value);
-}
+extern "C" double mavka_round(double value) { return round(value); }
 
-extern "C" void* mavka_malloc(size_t size) {
-  return malloc(size);
-}
+extern "C" void *mavka_malloc(size_t size) { return malloc(size); }
 
-extern "C" void* mavka_realloc(void* ptr, size_t size) {
+extern "C" void *mavka_realloc(void *ptr, size_t size) {
   return realloc(ptr, size);
 }
 
-extern "C" void mavka_free(void* ptr) {
-  free(ptr);
-}
+extern "C" void mavka_free(void *ptr) { free(ptr); }
 
-extern "C" void mavka_exit(int value) {
-  exit(value);
-}
+extern "C" void mavka_exit(int value) { exit(value); }
 
-extern "C" void mavka_print_utf8(char* value, size_t length) {
+extern "C" void mavka_print_utf8(char *value, size_t length) {
   printf("%.*s", (int)length, value);
 }
 
-extern "C" size_t mavka_double_to_string(double value, char** buffer) {
+extern "C" size_t mavka_double_to_string(double value, char **buffer) {
   long decimal = (long)value;
   if (decimal == value) {
-    *buffer = (char*)malloc(32);
-    return sprintf((char*)*buffer, "%ld", decimal);
+    *buffer = (char *)malloc(32);
+    return sprintf((char *)*buffer, "%ld", decimal);
   }
-  *buffer = (char*)malloc(32);
-  return sprintf((char*)*buffer, "%.14f", value);
+  *buffer = (char *)malloc(32);
+  return sprintf((char *)*buffer, "%.14f", value);
 }
 
 extern "C" double mavka_bitnot(double value) {
@@ -227,6 +195,4 @@ extern "C" double mavka_bitnot(double value) {
   return static_cast<double>(~int_value);
 }
 
-extern "C" double mavka_negate(double value) {
-  return -value;
-}
+extern "C" double mavka_negate(double value) { return -value; }
--- external/mavka_parser/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-cd syntax
-java -jar antlr-4.13.0-complete.jar -Dlanguage=Cpp *.g4 -visitor -o generated
-cd ..
-exit 0
