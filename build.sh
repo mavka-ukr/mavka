cf85ca5d69638e18cb663dee3ae75396a3f94d47
Davyd Kohut <davyd.kohut@gmail.com>
2025-04-05 21:25:59 +0300

    я знову все переписав

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+set -x
+
+TSIL="ціль" sh external/scripts/prepare.sh linux-x86_64
+cd build/linux-x86_64
+sh build.sh debug
+cd ../../
+mkdir -p out
+cp build/linux-x86_64/out/мавка out/мавка
