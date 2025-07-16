d3a9a02c9afd9a663d3c1d1b0fb0a86d6c849541
Davyd Kohut <davyd.kohut@gmail.com>
2025-07-17 00:53:19 +0300

    Мавка 0.121.3

--- external/scripts/linux-x86_64/package.sh
+++ external/scripts/linux-x86_64/package.sh
@@ -11,10 +11,10 @@ mkdir -p "package/мавка-$MAVKA_VERSION-linux-x86_64-prepared"
 cp "РМв1.в.ц" "package/мавка-$MAVKA_VERSION-linux-x86_64/include/РМв1.в.ц"
 cp "out/мавка" "package/мавка-$MAVKA_VERSION-linux-x86_64/bin/мавка"
 cd "package"
-tar -czvf "мавка-$MAVKA_VERSION-linux-x86_64.tar.gz" "мавка-$MAVKA_VERSION-linux-x86_64"
+tar -cJvf "мавка-$MAVKA_VERSION-linux-x86_64.tar.xz" "мавка-$MAVKA_VERSION-linux-x86_64"
 cd -
 
 cp -a source build.sh package.sh SourceFiles Version "package/мавка-$MAVKA_VERSION-linux-x86_64-prepared"
 cd "package"
-tar -czvf "мавка-$MAVKA_VERSION-linux-x86_64-prepared.tar.gz" "мавка-$MAVKA_VERSION-linux-x86_64-prepared"
+tar -cJvf "мавка-$MAVKA_VERSION-linux-x86_64-prepared.tar.xz" "мавка-$MAVKA_VERSION-linux-x86_64-prepared"
 cd -
