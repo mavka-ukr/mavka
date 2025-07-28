c769213d5dab56a0da300f7e58022d69411eec78
Davyd Kohut <davyd.kohut@gmail.com>
2025-07-28 23:57:58 +0300

    🧬

--- external/scripts/linux-x86_64/package.sh
+++ external/scripts/linux-x86_64/package.sh
@@ -5,10 +5,9 @@ set -x
 MAVKA_VERSION="$(cat Version)"
 
 mkdir -p "package/мавка-$MAVKA_VERSION-linux-x86_64/bin"
-mkdir -p "package/мавка-$MAVKA_VERSION-linux-x86_64/include"
 mkdir -p "package/мавка-$MAVKA_VERSION-linux-x86_64-prepared"
 
-cp "РМв1.в.ц" "package/мавка-$MAVKA_VERSION-linux-x86_64/include/РМв1.в.ц"
+cp -a "визначення" "package/мавка-$MAVKA_VERSION-linux-x86_64"
 cp "out/мавка" "package/мавка-$MAVKA_VERSION-linux-x86_64/bin/мавка"
 cd "package"
 tar -cJvf "мавка-$MAVKA_VERSION-linux-x86_64.tar.xz" "мавка-$MAVKA_VERSION-linux-x86_64"
--- external/scripts/prepare.sh
+++ external/scripts/prepare.sh
@@ -69,6 +69,9 @@ CompilationFiles_HEREDOC_INPUT
 Command="cp $BuildFilesDir/* $BuildDir"
 echo "$Command"
 $Command
+Command="cp -a визначення $BuildDir"
+echo "$Command"
+$Command
 Command="cp Версія $BuildDir/Version"
 echo "$Command"
 $Command
