2559fcda114cfdaa4dbb196e7c0f50155504381f
Davyd Kohut <davyd.kohut@gmail.com>
2025-12-01 07:57:40 +0200

    🧬

--- build.sh
+++ build.sh
@@ -2,16 +2,8 @@
 set -e
 
 MAVKA_VERSION=$(cat ВЕРСІЯ)
-
 BUILD_MODE="$1"
 BUILD_PLATFORM="$2"
-BUILD_ARCH=""
-BUILD_SYSTEM=""
-COMMON_SYSTEM=""
-TARGET_TRIPLE=""
-TSIL_ARCH=""
-TSIL_SYSTEM=""
-OUTFILENAME=""
 
 TSIL="ціль"
 CLANG="clang"
@@ -20,101 +12,78 @@ LLIRFILES=""
 
 print_usage() {
   echo "Usage: $0 <debug|release> <platform>"
-  echo "Supported platforms: x86_64-linux, aarch64-linux, x86_64-windows, aarch64-windows, x86_64-macos, aarch64-macos"
+  echo "Supported platforms: linux-x86_64, linux-aarch64, macos-x86_64, macos-aarch64, windows-x86_64, windows-aarch64"
 }
 
-if [ -z "$BUILD_MODE" ]; then
+set_build_mode() {
+  local mode="$1"
+
+  case "$mode" in
+    debug)
+      CLANG_OPTIONS+=" -g -O0" ;;
+    release)
+      CLANG_OPTIONS+=" -O3" ;;
+    *)
+      echo "Unsupported build mode: $mode"
+      print_usage
+      exit 1 ;;
+  esac
+}
+
+set_platform_vars() {
+  local platform="$1"
+  local system arch common_sys target tsil_system tsil_arch outfile clang_bin extra_opts
+
+  case "$platform" in
+    linux-x86_64)
+      system="linux"; arch="x86_64"; common_sys="unix"; target="x86_64-pc-linux-gnu"
+      tsil_system="лінукс"; tsil_arch="ікс86_64"; outfile="мавка"; clang_bin="clang"; extra_opts="-lm" ;;
+    linux-aarch64)
+      system="linux"; arch="aarch64"; common_sys="unix"; target="aarch64-linux-gnu"
+      tsil_system="лінукс"; tsil_arch="аарч64"; outfile="мавка"; clang_bin="zig cc"; extra_opts="-lm" ;;
+    macos-x86_64)
+      system="macos"; arch="x86_64"; common_sys="unix"; target="x86_64-macos"
+      tsil_system="макос"; tsil_arch="ікс86_64"; outfile="мавка"; clang_bin="zig cc"; extra_opts="-lm" ;;
+    macos-aarch64)
+      system="macos"; arch="aarch64"; common_sys="unix"; target="aarch64-macos"
+      tsil_system="макос"; tsil_arch="аарч64"; outfile="мавка"; clang_bin="zig cc"; extra_opts="-lm" ;;
+    windows-x86_64)
+      system="windows"; arch="x86_64"; common_sys="windows"; target="x86_64-windows-gnu"
+      tsil_system="віндовс"; tsil_arch="ікс86_64"; outfile="мавка.exe"; clang_bin="zig cc"; extra_opts="" ;;
+    windows-aarch64)
+      system="windows"; arch="aarch64"; common_sys="windows"; target="aarch64-windows-gnu"
+      tsil_system="віндовс"; tsil_arch="аарч64"; outfile="мавка.exe"; clang_bin="zig cc"; extra_opts="" ;;
+    *)
+      echo "Unsupported build platform: $platform"
+      exit 1 ;;
+  esac
+
+  BUILD_SYSTEM="$system"
+  BUILD_ARCH="$arch"
+  COMMON_SYSTEM="$common_sys"
+  TARGET_TRIPLE="$target"
+  TSIL_SYSTEM="$tsil_system"
+  TSIL_ARCH="$tsil_arch"
+  OUTFILENAME="$outfile"
+  CLANG="$clang_bin"
+  CLANG_OPTIONS+=" $extra_opts"
+}
+
+if [ -z "$BUILD_MODE" ] || [ -z "$BUILD_PLATFORM" ]; then
   print_usage
   exit 1
 fi
 
-if [ -z "$BUILD_PLATFORM" ]; then
-  print_usage
-  exit 1
-fi
-
-if [[ "$BUILD_MODE" == "debug" ]]; then
-  CLANG_OPTIONS+=" -g -O0"
-elif [[ "$BUILD_MODE" == "release" ]]; then
-  CLANG_OPTIONS+=" -O3"
-else
-  echo "Unsupported build mode: $BUILD_MODE"
-  print_usage
-  exit 1
-fi
-
-if [[ "$BUILD_PLATFORM" == "x86_64-linux" ]]; then
-  BUILD_ARCH="x86_64"
-  BUILD_SYSTEM="linux"
-  COMMON_SYSTEM="unix"
-  TARGET_TRIPLE="x86_64-pc-linux-gnu"
-  TSIL_ARCH="ікс86_64"
-  TSIL_SYSTEM="лінукс"
-  OUTFILENAME="мавка"
-  CLANG_OPTIONS+=" -lm"
-elif [[ "$BUILD_PLATFORM" == "aarch64-linux" ]]; then
-  BUILD_ARCH="aarch64"
-  BUILD_SYSTEM="linux"
-  COMMON_SYSTEM="unix"
-  TARGET_TRIPLE="aarch64-linux-gnu"
-  TSIL_ARCH="аарч64"
-  TSIL_SYSTEM="лінукс"
-  CLANG="zig cc"
-  OUTFILENAME="мавка"
-  CLANG_OPTIONS+=" -lm"
-elif [[ "$BUILD_PLATFORM" == "x86_64-windows" ]]; then
-  BUILD_ARCH="x86_64"
-  BUILD_SYSTEM="windows"
-  COMMON_SYSTEM="windows"
-  TARGET_TRIPLE="x86_64-windows-gnu"
-  TSIL_ARCH="ікс86_64"
-  TSIL_SYSTEM="віндовс"
-  CLANG="zig cc"
-  OUTFILENAME="мавка.exe"
-elif [[ "$BUILD_PLATFORM" == "aarch64-windows" ]]; then
-  BUILD_ARCH="aarch64"
-  BUILD_SYSTEM="windows"
-  COMMON_SYSTEM="windows"
-  TARGET_TRIPLE="aarch64-windows-gnu"
-  TSIL_ARCH="аарч64"
-  TSIL_SYSTEM="віндовс"
-  CLANG="zig cc"
-  OUTFILENAME="мавка.exe"
-elif [[ "$BUILD_PLATFORM" == "x86_64-macos" ]]; then
-  BUILD_ARCH="x86_64"
-  BUILD_SYSTEM="macos"
-  COMMON_SYSTEM="unix"
-  TARGET_TRIPLE="x86_64-macos"
-  TSIL_ARCH="ікс86_64"
-  TSIL_SYSTEM="макос"
-  CLANG="zig cc"
-  OUTFILENAME="мавка"
-  CLANG_OPTIONS+=" -lm"
-elif [[ "$BUILD_PLATFORM" == "aarch64-macos" ]]; then
-  BUILD_ARCH="aarch64"
-  BUILD_SYSTEM="macos"
-  COMMON_SYSTEM="unix"
-  TARGET_TRIPLE="aarch64-macos"
-  TSIL_ARCH="аарч64"
-  TSIL_SYSTEM="макос"
-  CLANG="zig cc"
-  OUTFILENAME="мавка"
-  CLANG_OPTIONS+=" -lm"
-else
-  echo "Unsupported build platform: $BUILD_PLATFORM"
-  exit 1
-fi
+set_build_mode "$BUILD_MODE"
+set_platform_vars "$BUILD_PLATFORM"
 
 SEMIREADY_DIR="будування/$MAVKA_VERSION/$TSIL_SYSTEM-$TSIL_ARCH/напівготове"
 READY_DIR="будування/$MAVKA_VERSION/$TSIL_SYSTEM-$TSIL_ARCH/готове"
 
-mkdir -p "$SEMIREADY_DIR"/бібліотека
-mkdir -p "$SEMIREADY_DIR"/КД
-mkdir -p "$SEMIREADY_DIR"/машина/предмети
-mkdir -p "$SEMIREADY_DIR"/перетворювач
-mkdir -p "$SEMIREADY_DIR"/пристрій
-mkdir -p "$SEMIREADY_DIR"/розбирач
-mkdir -p "$READY_DIR"
+prepare_directories() {
+  mkdir -p "$SEMIREADY_DIR"/{бібліотека,КД,машина/предмети,перетворювач,пристрій,розбирач}
+  mkdir -p "$READY_DIR"
+}
 
 compile_tsil() {
   local input_file="$1"
@@ -126,62 +95,70 @@ compile_tsil() {
   LLIRFILES+=" $SEMIREADY_DIR/$input_file.ллвмір.ll"
 }
 
-compile_tsil "бібліотека/бібліотека.ц"
-compile_tsil "бібліотека/математика.ц"
-compile_tsil "КД/КД.ц"
-compile_tsil "машина/ВзятіЗначення.ц"
-compile_tsil "машина/Властивості.ц"
-compile_tsil "машина/Дійсність.ц"
-compile_tsil "машина/Допоміжне.ц"
-compile_tsil "машина/Задіяні.ц"
-compile_tsil "машина/Значення.ц"
-compile_tsil "машина/ІменованоЗадіяні.ц"
-compile_tsil "машина/ІнформаціяПадіння.ц"
-compile_tsil "машина/КористувацькіДані.ц"
-compile_tsil "машина/Машина.ц"
-compile_tsil "машина/МісцезнаходженняВказівок.ц"
-compile_tsil "машина/НакопичувачТексту.ц"
-compile_tsil "машина/Очищувач.ц"
-compile_tsil "машина/Притримувач.ц"
-compile_tsil "машина/Спроби.ц"
-compile_tsil "машина/предмети/ПредметДаних.ц"
-compile_tsil "машина/предмети/ПредметДіапазонуДробових.ц"
-compile_tsil "машина/предмети/ПредметДіапазонуЦілих.ц"
-compile_tsil "машина/предмети/ПредметДії.ц"
-compile_tsil "машина/предмети/ПредметДробового.ц"
-compile_tsil "машина/предмети/ПредметЗміннихДаних.ц"
-compile_tsil "машина/предмети/ПредметКоду.ц"
-compile_tsil "машина/предмети/ПредметМодуля.ц"
-compile_tsil "машина/предмети/ПредметПараметра.ц"
-compile_tsil "машина/предмети/ПредметПереборуДіапазонуДробових.ц"
-compile_tsil "машина/предмети/ПредметПереборуДіапазонуЦілих.ц"
-compile_tsil "машина/предмети/ПредметПереборуСписку.ц"
-compile_tsil "машина/предмети/ПредметПослідовності.ц"
-compile_tsil "машина/предмети/ПредметСловника.ц"
-compile_tsil "машина/предмети/ПредметСписку.ц"
-compile_tsil "машина/предмети/ПредметСтруктури.ц"
-compile_tsil "машина/предмети/ПредметТексту.ц"
-compile_tsil "машина/предмети/ПредметЦілого.ц"
-compile_tsil "машина/предмети/СкладенийПредмет.ц"
-compile_tsil "перетворювач/перетворювач.ц"
-compile_tsil "пристрій/взяти_файл.ц"
-compile_tsil "пристрій/вивід.ц"
-compile_tsil "пристрій/глобальні.ц"
-compile_tsil "пристрій/машина.ц"
-compile_tsil "пристрій/пристрій.ц"
-compile_tsil "розбирач/розбирач.ц"
-compile_tsil "старт.ц"
+compile_all_tsil_files() {
+  compile_tsil "бібліотека/бібліотека.ц"
+  compile_tsil "бібліотека/математика.ц"
+  compile_tsil "КД/КД.ц"
+  compile_tsil "машина/ВзятіЗначення.ц"
+  compile_tsil "машина/Властивості.ц"
+  compile_tsil "машина/Дійсність.ц"
+  compile_tsil "машина/Допоміжне.ц"
+  compile_tsil "машина/Задіяні.ц"
+  compile_tsil "машина/Значення.ц"
+  compile_tsil "машина/ІменованоЗадіяні.ц"
+  compile_tsil "машина/ІнформаціяПадіння.ц"
+  compile_tsil "машина/КористувацькіДані.ц"
+  compile_tsil "машина/Машина.ц"
+  compile_tsil "машина/МісцезнаходженняВказівок.ц"
+  compile_tsil "машина/НакопичувачТексту.ц"
+  compile_tsil "машина/Очищувач.ц"
+  compile_tsil "машина/Притримувач.ц"
+  compile_tsil "машина/Спроби.ц"
+  compile_tsil "машина/предмети/ПредметДаних.ц"
+  compile_tsil "машина/предмети/ПредметДіапазонуДробових.ц"
+  compile_tsil "машина/предмети/ПредметДіапазонуЦілих.ц"
+  compile_tsil "машина/предмети/ПредметДії.ц"
+  compile_tsil "машина/предмети/ПредметДробового.ц"
+  compile_tsil "машина/предмети/ПредметЗміннихДаних.ц"
+  compile_tsil "машина/предмети/ПредметКоду.ц"
+  compile_tsil "машина/предмети/ПредметМодуля.ц"
+  compile_tsil "машина/предмети/ПредметПараметра.ц"
+  compile_tsil "машина/предмети/ПредметПереборуДіапазонуДробових.ц"
+  compile_tsil "машина/предмети/ПредметПереборуДіапазонуЦілих.ц"
+  compile_tsil "машина/предмети/ПредметПереборуСписку.ц"
+  compile_tsil "машина/предмети/ПредметПослідовності.ц"
+  compile_tsil "машина/предмети/ПредметСловника.ц"
+  compile_tsil "машина/предмети/ПредметСписку.ц"
+  compile_tsil "машина/предмети/ПредметСтруктури.ц"
+  compile_tsil "машина/предмети/ПредметТексту.ц"
+  compile_tsil "машина/предмети/ПредметЦілого.ц"
+  compile_tsil "машина/предмети/СкладенийПредмет.ц"
+  compile_tsil "перетворювач/перетворювач.ц"
+  compile_tsil "пристрій/взяти_файл.ц"
+  compile_tsil "пристрій/вивід.ц"
+  compile_tsil "пристрій/глобальні.ц"
+  compile_tsil "пристрій/машина.ц"
+  compile_tsil "пристрій/пристрій.ц"
+  compile_tsil "розбирач/розбирач.ц"
+  compile_tsil "старт.ц"
+}
 
-echo "створення виконуваного файлу"
+link_executable() {
+  echo "створення виконуваного файлу"
 
-$CLANG --target="$TARGET_TRIPLE" \
-       $CLANG_OPTIONS \
-       -o "$READY_DIR"/"$OUTFILENAME" \
-       "external/$COMMON_SYSTEM/main_$COMMON_SYSTEM.c" \
-       "external/$COMMON_SYSTEM/prystriy_$COMMON_SYSTEM.c" \
-       "external/$COMMON_SYSTEM/biblioteka_$COMMON_SYSTEM.c" \
-       "external/$BUILD_SYSTEM/biblioteka_$BUILD_SYSTEM.c" \
-       $LLIRFILES
+  $CLANG --target="$TARGET_TRIPLE" \
+         $CLANG_OPTIONS \
+         -o "$READY_DIR/$OUTFILENAME" \
+         "external/$COMMON_SYSTEM/main_$COMMON_SYSTEM.c" \
+         "external/$COMMON_SYSTEM/prystriy_$COMMON_SYSTEM.c" \
+         "external/$COMMON_SYSTEM/biblioteka_$COMMON_SYSTEM.c" \
+         "external/$BUILD_SYSTEM/biblioteka_$BUILD_SYSTEM.c" \
+         $LLIRFILES
 
-echo "готово!"
-echo "виконуваний файл: $READY_DIR/$OUTFILENAME"
+  echo "готово!"
+  echo "виконуваний файл: $READY_DIR/$OUTFILENAME"
+}
+
+prepare_directories
+compile_all_tsil_files
+link_executable
--- package.sh
+++ package.sh
@@ -2,19 +2,41 @@
 set -e
 
 RUNDIR="$PWD"
-
 MAVKA_VERSION=$(cat ВЕРСІЯ)
-
 BUILD_PLATFORM="$1"
-BUILD_ARCH=""
-BUILD_SYSTEM=""
-TSIL_ARCH=""
-TSIL_SYSTEM=""
-OUTFILENAME=""
 
 print_usage() {
   echo "Usage: $0 <platform>"
-  echo "Supported platforms: x86_64-linux, aarch64-linux, x86_64-windows, aarch64-windows, x86_64-macos, aarch64-macos"
+  echo "Supported platforms: linux-x86_64, linux-aarch64, macos-x86_64, macos-aarch64, windows-x86_64, windows-aarch64"
+}
+
+set_platform_vars() {
+  local platform="$1"
+  local system arch tsil_system tsil_arch outfile
+
+  case "$platform" in
+    linux-x86_64)
+      system="linux"; arch="x86_64"; tsil_system="лінукс"; tsil_arch="ікс86_64"; outfile="мавка" ;;
+    linux-aarch64)
+      system="linux"; arch="aarch64"; tsil_system="лінукс"; tsil_arch="аарч64"; outfile="мавка" ;;
+    macos-x86_64)
+      system="macos"; arch="x86_64"; tsil_system="макос"; tsil_arch="ікс86_64"; outfile="мавка" ;;
+    macos-aarch64)
+      system="macos"; arch="aarch64"; tsil_system="макос"; tsil_arch="аарч64"; outfile="мавка" ;;
+    windows-x86_64)
+      system="windows"; arch="x86_64"; tsil_system="віндовс"; tsil_arch="ікс86_64"; outfile="мавка.exe" ;;
+    windows-aarch64)
+      system="windows"; arch="aarch64"; tsil_system="віндовс"; tsil_arch="аарч64"; outfile="мавка.exe" ;;
+    *)
+      echo "Unsupported build platform: $platform"
+      exit 1 ;;
+  esac
+
+  BUILD_SYSTEM="$system"
+  BUILD_ARCH="$arch"
+  TSIL_SYSTEM="$tsil_system"
+  TSIL_ARCH="$tsil_arch"
+  OUTFILENAME="$outfile"
 }
 
 if [ -z "$BUILD_PLATFORM" ]; then
@@ -22,46 +44,7 @@ if [ -z "$BUILD_PLATFORM" ]; then
   exit 1
 fi
 
-if [[ "$BUILD_PLATFORM" == "x86_64-linux" ]]; then
-  BUILD_ARCH="x86_64"
-  BUILD_SYSTEM="linux"
-  TSIL_ARCH="ікс86_64"
-  TSIL_SYSTEM="лінукс"
-  OUTFILENAME="мавка"
-elif [[ "$BUILD_PLATFORM" == "aarch64-linux" ]]; then
-  BUILD_ARCH="aarch64"
-  BUILD_SYSTEM="linux"
-  TSIL_ARCH="аарч64"
-  TSIL_SYSTEM="лінукс"
-  OUTFILENAME="мавка"
-elif [[ "$BUILD_PLATFORM" == "x86_64-windows" ]]; then
-  BUILD_ARCH="x86_64"
-  BUILD_SYSTEM="windows"
-  TSIL_ARCH="ікс86_64"
-  TSIL_SYSTEM="віндовс"
-  OUTFILENAME="мавка.exe"
-elif [[ "$BUILD_PLATFORM" == "aarch64-windows" ]]; then
-  BUILD_ARCH="aarch64"
-  BUILD_SYSTEM="windows"
-  TSIL_ARCH="аарч64"
-  TSIL_SYSTEM="віндовс"
-  OUTFILENAME="мавка.exe"
-elif [[ "$BUILD_PLATFORM" == "x86_64-macos" ]]; then
-  BUILD_ARCH="x86_64"
-  BUILD_SYSTEM="macos"
-  TSIL_ARCH="ікс86_64"
-  TSIL_SYSTEM="макос"
-  OUTFILENAME="мавка"
-elif [[ "$BUILD_PLATFORM" == "aarch64-macos" ]]; then
-  BUILD_ARCH="aarch64"
-  BUILD_SYSTEM="macos"
-  TSIL_ARCH="аарч64"
-  TSIL_SYSTEM="макос"
-  OUTFILENAME="мавка"
-else
-  echo "Unsupported build platform: $BUILD_PLATFORM"
-  exit 1
-fi
+set_platform_vars "$BUILD_PLATFORM"
 
 VERSIONDIR="випуски/$MAVKA_VERSION"
 PLATFORMDIRNAME="мавка-$MAVKA_VERSION-$TSIL_SYSTEM-$TSIL_ARCH"
@@ -73,66 +56,70 @@ if [ -d "$VERSIONDIR" ]; then
   exit 1
 fi
 
-if [[ "$BUILD_SYSTEM" == "linux" || "$BUILD_SYSTEM" == "macos" ]]; then
-  BINDIR="$PLATFORMDIR/bin"
-fi
+[[ "$BUILD_SYSTEM" == "linux" || "$BUILD_SYSTEM" == "macos" ]] && BINDIR="$PLATFORMDIR/bin"
 
-mkdir -p "$VERSIONDIR"
-mkdir -p "$PLATFORMDIR"
 mkdir -p "$BINDIR"
 
-tar -cf "$VERSIONDIR/мавка-$MAVKA_VERSION.tar" --exclude=.git --exclude-vcs --exclude-from=.gitignore --transform="s|^\.|мавка-$MAVKA_VERSION|" .
-tar -rf "$VERSIONDIR/мавка-$MAVKA_VERSION.tar" --transform="s|^\.gitignore|мавка-$MAVKA_VERSION/.gitignore|" .gitignore
-xz -z "$VERSIONDIR/мавка-$MAVKA_VERSION.tar"
+create_source_tarball() {
+  local tarball="$VERSIONDIR/мавка-$MAVKA_VERSION.tar"
+  tar -cf "$tarball" --exclude=.git --exclude-vcs --exclude-from=.gitignore --transform="s|^\.|мавка-$MAVKA_VERSION|" .
+  tar -rf "$tarball" --transform="s|^\.gitignore|мавка-$MAVKA_VERSION/.gitignore|" .gitignore
+  xz -z "$tarball"
+}
 
-cp "будування/$MAVKA_VERSION/$TSIL_SYSTEM-$TSIL_ARCH/готове/$OUTFILENAME" "$BINDIR"
+prepare_platform_dir() {
+  cp "будування/$MAVKA_VERSION/$TSIL_SYSTEM-$TSIL_ARCH/готове/$OUTFILENAME" "$BINDIR"
+  cp -a "означення" "$PLATFORMDIR/означення"
+  cp "КД/КД.о.ц" "$PLATFORMDIR/означення"
+}
 
-cp -a "означення" "$PLATFORMDIR/означення"
-cp "КД/КД.о.ц" "$PLATFORMDIR/означення"
+create_platform_tarball() {
+  cd "$VERSIONDIR"
+  tar -cJvf "мавка-$MAVKA_VERSION-$TSIL_SYSTEM-$TSIL_ARCH.tar.xz" "$PLATFORMDIRNAME"
+  rm -rf "$PLATFORMDIRNAME"
+  cd - > /dev/null
+}
 
-cd "$VERSIONDIR"
+create_source_tarball
+prepare_platform_dir
+create_platform_tarball
 
-tar -cJvf "мавка-$MAVKA_VERSION-$TSIL_SYSTEM-$TSIL_ARCH.tar.xz" "$PLATFORMDIRNAME"
+get_gpg_fingerprint() {
+  gpg --with-colons --list-secret-keys --fingerprint | awk -F: '/^fpr:/ {print $10; exit}'
+}
 
-rm -rf "$PLATFORMDIRNAME"
+sign_release_files() {
+  local private_key="$RUNDIR/.releasegpgkey"
+  local passphrase_file="$RUNDIR/.releasegpgkeypassphrase"
+  local tmp_gpg_home=$(mktemp -d)
 
-PRIVATE_KEY_FILE="$RUNDIR/.releasegpgkey"
-PRIVATE_KEY_FILE_PASSPHRASE="$RUNDIR/.releasegpgkeypassphrase"
-TMP_GPG_HOME=$(mktemp -d)
+  export GNUPGHOME="$tmp_gpg_home"
 
-export GNUPGHOME="$TMP_GPG_HOME"
+  gpg --batch --yes --import "$private_key" >/dev/null 2>&1
 
-# Import the private key into temporary keyring
-gpg --batch --yes --import "$PRIVATE_KEY_FILE" >/dev/null 2>&1
-
-# Get fingerprint from this keyring
-FINGERPRINT=$(gpg --with-colons --list-secret-keys --fingerprint \
-  | awk -F: '/^fpr:/ {print $10; exit}')
-
-if [ -z "$FINGERPRINT" ]; then
-  echo "Failed to get fingerprint (no secret key found)"
-  exit 1
-fi
-
-echo "Using fingerprint: $FINGERPRINT"
-
-# Read passphrase from file (trim spaces/newlines)
-PASSPHRASE=$(<"$PRIVATE_KEY_FILE_PASSPHRASE")
-
-for file in мавка-"$MAVKA_VERSION"-$TSIL_SYSTEM-$TSIL_ARCH.tar.xz мавка-"$MAVKA_VERSION".tar.xz; do
-  if [ ! -f "$file" ]; then
-    echo "File not found: $file"
-    continue
+  local fingerprint=$(get_gpg_fingerprint)
+  if [ -z "$fingerprint" ]; then
+    echo "Failed to get fingerprint (no secret key found)"
+    rm -rf "$tmp_gpg_home"
+    exit 1
   fi
 
-  sha256sum "$file" > "$file.sha256"
+  echo "Using fingerprint: $fingerprint"
 
-  # Sign using the imported key ONLY inside this GNUPGHOME
-  gpg --batch --yes --pinentry-mode loopback --passphrase "$PASSPHRASE" \
-    --local-user "$FINGERPRINT" --clearsign --output "$file.sha256.signed" "$file.sha256"
-done
+  local passphrase=$(<"$passphrase_file")
 
-# Clean up the temporary keyring
-rm -rf "$TMP_GPG_HOME"
+  cd "$VERSIONDIR"
 
-cd -
+  for file in мавка-"$MAVKA_VERSION"-$TSIL_SYSTEM-$TSIL_ARCH.tar.xz мавка-"$MAVKA_VERSION".tar.xz; do
+    [ ! -f "$file" ] && { echo "File not found: $file"; continue; }
+
+    sha256sum "$file" > "$file.sha256"
+    gpg --batch --yes --pinentry-mode loopback --passphrase "$passphrase" \
+      --local-user "$fingerprint" --clearsign --output "$file.sha256.signed" "$file.sha256"
+  done
+
+  cd - > /dev/null
+  rm -rf "$tmp_gpg_home"
+}
+
+sign_release_files
