b67dd151d5e82c54915c404fab6c53d4fc588a5c
Davyd Kohut <davyd.kohut@gmail.com>
2025-12-08 20:08:50 +0200

    🧬

--- scripts/build.sh
+++ scripts/build.sh
@@ -349,7 +349,7 @@ compile_tsil() {
   $TSIL \
     "$(realpath --relative-to="$(pwd)" "$SEMIREADY_DIR/$input_file.ллвмір")" -П="$TSIL_PLATFORM" \
     перетворити \
-    "$(realpath --relative-to="$(pwd)" "$input_file")" -О=./КД/означення -О=./бібліотека/означення -О=./означення
+    "$(realpath --relative-to="$(pwd)" "$input_file")" -О=./бібліотека/означення -О=./означення
   mv "$SEMIREADY_DIR/$input_file.ллвмір" "$SEMIREADY_DIR/$input_file.ллвмір".ll
 
   LLIRFILES+=" $SEMIREADY_DIR/$input_file.ллвмір.ll"
