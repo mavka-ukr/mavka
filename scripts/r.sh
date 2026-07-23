3ea1f829d082f875fa165ea406ef4eb5f51e35a2
Давид <davyd.kohut@gmail.com>
2026-07-24 00:44:46 +0300

    🧬

---
+++ scripts/r.sh
@@ -0,0 +1,5 @@
+export ANDROID_NDK_HOME="/home/$USER/Compilers/Android/android-ndk-r27d"
+export LLVM_MINGW_PATH="/home/$USER/Compilers/llvm-mingw-20260616-ucrt-ubuntu-22.04-x86_64"
+export OSXCROSS_PATH="/home/$USER/Compilers/osxcross/target"
+
+bash scripts/release.sh
